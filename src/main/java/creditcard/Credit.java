98768b27f36b0d66e0d5b7280f72600065b5b0c4
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-02-28 09:44:31 +0530

    commit

---
+++ src/main/java/creditcard/Credit.java
@@ -0,0 +1,29 @@
+package creditcard;
+
+import java.util.logging.Logger;
+
+class Credit implements Cloneable{
+	String cardholder;
+	Long cardnumber;
+	String expirydate;
+	Logger log=Logger.getLogger("hi");
+	Credit(String cardholder,Long cardnumber,String expirydate)
+	{
+		this.cardholder=cardholder;
+		this.cardnumber=cardnumber;
+		this.expirydate=expirydate;
+	}
+	Credit active() {
+		try{
+			return (Credit) super.clone();
+		}
+		catch(Exception e) {
+			log.info("error");
+			return this;
+		}
+	}
+	boolean helo(Long card)
+	{
+		return cardnumber.equals(card);
+	}
+}
