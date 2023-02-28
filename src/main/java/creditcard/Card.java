137f2da974fe01823bc6babc24817240806439a0
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-02-28 09:43:41 +0530

    comit

--- src/main/java/creditcard/Card.java
+++ src/main/java/creditcard/Card.java
@@ -1,31 +1,6 @@
 package creditcard;
 import java.util.Scanner;
 import java.util.logging.*;
-class Credit implements cloneable{
-	String cardholder;
-	Long cardnumber;
-	String expirydate;
-	Logger log=Logger.getLogger("hi");
-	Credit(String cardholder,Long cardnumber,String expirydate)
-	{
-		this.cardholder=cardholder;
-		this.cardnumber=cardnumber;
-		this.expirydate=expirydate;
-	}
-	Credit active() {
-		try{
-			return (Credit) super.clone();
-		}
-		catch(Exception e) {
-			log.info("error");
-			return this;
-		}
-	}
-	boolean helo(Long card)
-	{
-		return cardnumber.equals(card);
-	}
-}
 public class Card
 {
     private static Scanner scan;
